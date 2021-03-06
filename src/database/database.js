996b699f0b51a6dac59d906f5d108aca8ee481e6
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-05 22:35:27 -0600

    new configure mongoose

--- src/database/database.js
+++ src/database/database.js
@@ -1,6 +1,8 @@
 var mongoose = require('mongoose');
 var config = require('../config/configapp');
 
+mongoose.Promise = global.Promise
+
 mongoose.connect(config.mongodbURL, {
     useNewUrlParser: true,
     useUnifiedTopology: true
