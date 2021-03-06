3665ca930c1e551d60fd82c752c8b68713be350c
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-05 23:09:43 -0600

    new configure mongoose

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var config = require('../config/configapp');
 
-mongoose.Promise = global.Promise
+mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb+srv://ged:Ged123@cluster0.qd2xq.mongodb.net/productoFP?retryWrites=true&w=majority', {
 
