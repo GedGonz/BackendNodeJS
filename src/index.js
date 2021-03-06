56e92c8ea2e35cafe0e5def81cf2d12afd2120be
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-05 23:14:24 -0600

    new configure mongoose

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 var multer = require('multer');
 var app = express();
 
-app.set('port', process.env.PORT || 3000)
+app.set('port', process.env.PORT || 5000)
 
 require('./database/database');
 
