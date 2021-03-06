e719db6e5ced0d5d6821c1300f0b91052f9cbf48
Gerald Gonzalez <gedgonz7@gmail.com>
2021-03-06 00:18:45 -0600

    config cors

--- src/index.js
+++ src/index.js
@@ -21,13 +21,9 @@ const corsOpts = {
     ],
 };
 
-app.use(cors(corsOpts));
-
 app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
 
